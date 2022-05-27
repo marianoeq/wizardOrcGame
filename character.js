5d297ccb4eddb0e417c89c2bdbd70702eb015754
marian.eq <mariano.eq@gmail.com>
2022-05-27 13:53:56 +0200

    CurrentDiceScore key added to the object

--- character.js
+++ character.js
@@ -3,28 +3,37 @@ import { getDiceRollArray, getDicePlaceholderHtml } from "./utils.js";
 function Character(data) {
   Object.assign(this, data);
 
-  this.takeDamage = function (){
-    console.log(`${this.name} is damaged`)
-  }
-
-  this.diceArray = getDicePlaceholderHtml(this.diceCount)
-  
-  this.getDiceHtml = function () {
-    this.currentDiceScore = getDiceRollArray(this.diceCount)
-    this.diceArray= this.currentDiceScore.map((num)=>{
-      return `<div class="dice">${num}</div>`
-        }).join("")
+  this.takeDamage = function (attackScoreArray) {
+    const totalAttackScore = attackScoreArray.reduce((cur, acc) => {
+      return cur + acc;
+    });
+    this.health = this.health - totalAttackScore;
+    if (this.health <= 0) {
+      this.dead = true;
+      this.health = 0;
+        }
   };
-  
+
+  this.diceArray = getDicePlaceholderHtml(this.diceCount);
+
+  this.getDiceHtml = function () {
+    this.currentDiceScore = getDiceRollArray(this.diceCount);
+    this.diceArray = this.currentDiceScore
+      .map((num) => {
+        return `<div class="dice">${num}</div>`;
+      })
+      .join("");
+  };
+
   this.getCharacterHtml = function () {
     const { name, avatar, health, diceArray } = this;
-   
+
     return `<div class= "character-card" >
                   <h4 class="name">${name}</h4>
                   <img class="avatar" src="${avatar}"/>
                   <p class="health">health: <b> ${health} </b></p>
                   <div class="dice-container"> ${diceArray}</div>
-              </div>`;
+            </div>`;
   };
 }
 
