fc7372603cf26ff26171243d4470ecd4cbc95370
marian.eq <mariano.eq@gmail.com>
2022-05-27 13:54:20 +0200

    endGame() function added

--- index.js
+++ index.js
@@ -13,10 +13,29 @@ render();
 document.getElementById("attack-button").addEventListener("click", attack);
 
 function attack() {
-  wizard.getDiceHtml()
-  orc.getDiceHtml()
-  wizard.takeDamage()
-  orc.takeDamage()
+  wizard.getDiceHtml();
+  orc.getDiceHtml();
+  wizard.takeDamage(orc.currentDiceScore);
+  orc.takeDamage(wizard.currentDiceScore);
   render();
+  if (orc.dead || wizard.dead) {
+    endGame();
+  }
 }
 
+function endGame() {
+  const endEmoji = wizard.health > 0 ? "🔮" : "☠️";
+
+  const endMessage =
+    wizard.health > 0 && orc.health === 0
+      ? `No victors - all creatures are dead ${endEmoji}`
+      : wizard.health > 0
+      ? `The Wizard Wins ${endEmoji}`
+      : `The Orc is Victorious ${endEmoji}`;
+
+  return (document.body.innerHTML = `<div class="end-game">
+  <h2>Game Over</h2>
+  <h3>${endMessage}</h3>
+  <p class="end-emoji">${endEmoji}</p>
+  </div>`);
+}
