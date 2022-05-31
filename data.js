8623d7a93792f0d49dd9b843eafc8c713add2d43
marian.eq <mariano.eq@gmail.com>
2022-05-31 15:46:27 +0200

    New data added

--- data.js
+++ data.js
@@ -6,13 +6,31 @@ const characterData = {
     diceCount: 3,
     currentDiceScore: []
   },
-  monster: {
+  orc: {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
     diceCount: 1,
     currentDiceScore: []
   },
+  demon: {
+    name: "Demon",
+    avatar: "images/demon.png",
+    health: 25,
+    diceCount: 2,
+    currentDiceScore: []
+},
+goblin: {
+    name: "Goblin",
+    avatar: "images/goblin.png",
+    health: 20,
+    diceCount: 3,
+    currentDiceScore: []
+}
 };
 
 export default characterData;
+
+
+// when orc is dead, I have to render monster.
+  // if orc is dead. trigger a function that will render the new character.
